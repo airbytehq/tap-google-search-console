24d67a48b97f5fb9af6cd4bcdf76c9dd75abe1dd
vitaliizazmic <vitalii.vdovenko@zazmic.com>
2021-04-29 19:30:35 +0300

    Fixing install requirements

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(name='tap-google-search-console',
           'backoff==1.8.0',
           'requests==2.22.0',
           'singer-python==5.8.1',
-          'google-api-python-client=2.2.0'
+          'google-api-python-client==2.2.0'
       ],
       extras_require={
           'dev': [
